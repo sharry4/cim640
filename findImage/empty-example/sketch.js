7def1f1d0bb54e76550c419646573a7b4f0051ff
sharry4 <sharrytong4@gmail.com>
2017-11-14 14:52:27 -0500

    Final

--- findImage/empty-example/sketch.js
+++ findImage/empty-example/sketch.js
@@ -8,6 +8,10 @@ var targetPosY = 100;
 
 //var overlayArray = [false, false, false];
 
+var currentPosition = -1;
+
+var answer = 2;
+
 function setup() {
     createCanvas(600, 400);
     pics[0] = loadImage("assets/football.png");
@@ -32,15 +36,23 @@ function draw() {
     for (var i = 0; i < targetPosX.length; i++) {
         if (mouseX > targetPosX[i] - 100 && mouseX < targetPosX[i] + 100 && mouseY > targetPosY - 100 && mouseY < targetPosY + 100) {
             image(pics[i], targetPosX[i], targetPosY);
+            currentPosition = i;
 
         } else {
             rect(targetPosX[i], 100, 200, 200);
         }
     }
 
+    if(answer == currentPosition) {
+       console.log("You guessed correctly");
+        text("You guessed correctly", 10, 300);
+       }else if(currentPosition == 0 || currentPosition == 1) {
+           console.log("You guessed incorrectly");
+           text("You guessed incorrectly", 10, 300);
+       }
 
-
-
+currentPosition = -1;
+    
     //
     //    if (mouseX > targetPosX[1] - 100 && mouseX < targetPosX[1] + 100 && mouseY > targetPosY - 100 && mouseY < targetPosY + 100) {
     //        image(pics[1], targetPosX[1], targetPosY);
---
+++ hw/Final/readme.md
@@ -0,0 +1,14 @@
+# Final Project Ideas
+
+## 1. Snowman
+I will create a snowman with buttons for interaction (changing background, starting snowflake, some sound effect).
+
+![Snowman Image](https://pan.baidu.com/s/1kVrGbJ5)
+
+
+## 2.Collecting gold coins
+I will create a game with a little man collecting gold coins. Within the game, there will be sound effects, and maybe option to change the background, etc. 
+
+![catch gold coin example](https://goo.gl/images/4tNWQ7)
+
+## 3. 
